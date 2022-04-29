cf2174c42dc39ac789921a2144bb1b75e6f2713f
iamgirya <iamgirya@yandex.ru>
2022-04-29 16:55:00 +0300

    11.7

--- lab11.pl
+++ lab11.pl
@@ -68,3 +68,6 @@ maxDigitU(X,M):- X1 is X div 10,maxDigitU(X1,M1),M2 is X mod 10,(M2>M1, M is M2;
 maxDigitD(X,M):- maxDigitD(X,0,M).
 maxDigitD(0,M,M):-!.
 maxDigitD(X,Y,M):-M1 is X mod 10, X1 is X div 10,M1 > Y,!,maxDigitD(X1,M1,M); X2 is X div 10, maxDigitD(X2,Y,M).
+%17	
+sumOfDigWhatDivOn3(0,0):-!.
+sumOfDigWhatDivOn3(X,M):-X1 is X div 10,sumOfDigWhatDivOn3(X1,M1),M2 is X mod 10,(0 is M2 mod 3, M is M2+M1; M is M1),!.
